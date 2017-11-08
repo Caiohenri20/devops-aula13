919175eab4712d7bad2fd53246064ec4046b8729
Caiohenri20 <31082360+Caiohenri20@users.noreply.github.com>
2017-11-08 20:49:20 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -11,5 +11,9 @@ class Curso(models.Model):
     instituicao = models.CharField(max_length=200)
 
 class Vestibular(models.Model):
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=200)  
+    
+
+class Candidatos(models.Model):
+    nome = models.CharField(max_length=200)     
 
